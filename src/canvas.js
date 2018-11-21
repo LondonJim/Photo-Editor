ba5b0b70b85b2638545779efe3876b61edd709eb
James Sutherland <jrsutherland78@googlemail.com>
2018-11-21 22:15:56 +0000

    feat: invert uploaded images

--- src/canvas.js
+++ src/canvas.js
@@ -4,9 +4,11 @@ class Canvas {
     this.image = document.getElementById('SourceImage')
     this.canvas = document.getElementById('Canvas')
     this.context = this.canvas.getContext('2d')
+    this.canvas.width = 1
+    this.canvas.height = 1
   }
 
-  drawImage() {
+  drawImageToCanvas() {
     var width = this.image.naturalWidth
     var height = this.image.naturalHeight
     this.canvas.width = width
@@ -23,10 +25,20 @@ class Canvas {
     reader.onload = function(event) {
       this.image.src = event.target.result
       setTimeout(function(){
-        this.drawImage()
+        this.drawImageToCanvas()
       }.bind(this), 1)
     }.bind(this)
 
     reader.readAsDataURL(selectedFile)
+  }
+
+  invertImage() {
+    var imageData = this.context.getImageData(0, 0, this.canvas.width, this.canvas.height)
+    for (var i = 0; i < imageData.data.length; i+=4) {
+      imageData.data[i] = imageData.data[i] ^ 255
+      imageData.data[i+1] = imageData.data[i+1] ^ 255
+      imageData.data[i+2] = imageData.data[i+2] ^ 255
+    }
+    this.context.putImageData(imageData, 0, 0)
   }
 }
